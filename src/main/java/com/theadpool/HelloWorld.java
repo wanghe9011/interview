6c667e7978f700aa054e2c709b00561943a2be6c
wanghe9011 <wanghe9011@sina.com>
2022-06-05 14:04:37 +0800

    update

--- src/main/java/com/theadpool/HelloWorld.java
+++ src/main/java/com/theadpool/HelloWorld.java
@@ -6,28 +6,21 @@ import com.theadpool.task.EchoTask;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 public class HelloWorld {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         ExecutorService executorService = Executors.newSingleThreadExecutor(new NamedTheadFactory());
         String echoMessage = "你还好吗? ";
 
-        //单个线程执行
-        new Thread(() -> {
-            System.out.println(Thread.currentThread().getName() + ":" + echoMessage);
-        }).start();
-
+        //新启一个单线程执行
+        new Thread(() -> System.out.println(Thread.currentThread().getName() + ":" + echoMessage)).start();
 
         //线程池执行
-        Future<String> future = executorService.submit(new EchoTask(echoMessage));
-        try {
-            System.out.println(Thread.currentThread().getName() + ":" + future.get());
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        } catch (ExecutionException e) {
-            e.printStackTrace();
-        }
+        executorService.submit(new EchoTask(echoMessage)).get();
+
+        //主线程
+        System.out.println(Thread.currentThread().getName() + ":" + echoMessage);
+
     }
 }
